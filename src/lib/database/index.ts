a5006f0351a68c96d73057e1c0c3f3bcad31fe7c
RIFAT <mohammadrifat.hossain.rk@gmail.com>
2024-01-18 18:17:56 +0600

    error test

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -9,7 +9,6 @@ export const connectToDatabase = async () =>{
     if(!MONGODB_URI) throw new Error('MONGODB_URI is missing')
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI,{
-        dbName:"EpicEvents",
         bufferCommands:false,
     })
 
